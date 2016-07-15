c40ef7c403c9afc947d86f0072cb1649f1d32a89
Frank McCoy <frankmccoy.d@gmail.com>
2016-07-14 21:30:57 -0500

    simple stylesheet changes

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -1,13 +1,17 @@
 <!DOCTYPE html>
-<html>
+<html lang='en'>
   <head>
-    <meta charset="utf-8">
-    <title></title>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>@yield('page-title', 'Bistro Restaurant Manager')</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}" charset="utf-8">
+    @stack('head')
   </head>
   <body>
-    @yield('content')
+    @yield('body')
     <script src="{{ asset('js/jquery.js') }}"></script>
     <script src="{{ asset('js/bootstrap.js') }}"></script>
+    @stack('foot')
   </body>
 </html>
