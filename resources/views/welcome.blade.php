5893db825363cb4527f5c3e60e9da17b65e46718
Frank McCoy <frankmccoy.d@gmail.com>
2016-08-05 22:15:03 -0500

    quick

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,25 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
+@extends('master')
 
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+@push('head')
+  <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
 
-        <style>
-            html, body {
-                height: 100%;
-            }
+  <style>
 
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato', sans-serif;
-            }
+      .content {
+        font-weight: 100;
+        font-family: 'Lato', sans-serif;
+        text-align: center;
+        padding-top: 28%;
+      }
 
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
+      .title {
+        font-size: 96px;
+      }
+  </style>
+@endpush
 
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
-</html>
+@section('content')
+  <div class="content">
+      <div class="title">Laravel 5</div>
+  </div>
+@endsection
